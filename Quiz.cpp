0f2ba32a86aa62800c638efd4876804b792b5dad
Hamzah7 <iqbalh11@uni.coventry.ac.uk>
2017-08-30 13:02:55 +0100

    Update Quiz.cpp

--- Quiz.cpp
+++ Quiz.cpp
@@ -11,67 +11,65 @@ Quiz::~Quiz()
 }
 void Quiz::geography()
 {
- string ans_1;
+ int ans_1;
+ int correct_1 = 1;
  cout<<"What is erosion?";
+ cout << " 1.rock are worn away through the action of water" << "\n 2.rocks breaking down each other " << "\n 3.Trees roots cause damge to rocks" << endl;
  cin>>ans_1;
 
- string ans_2;
+ int ans_2;
+ int correct_2 = 3;
  cout<<"What are the name of the longest river in the world?";
+ cout << " 1.Congo river" << "\n 2. Amazon River " << "\n 3.River Nile" << endl;
  cin>>ans_2;
 
- string ans_3;
+ int ans_3;
+ int correct_3 = 2;
  cout<<"What is measured by the Richter scale?";
+ cout << " 1.Distance" << "\n 2. Earthquake " << "\n 3.Temperature" << endl;
  cin>>ans_3;
 
- string ans_4;
+ int ans_4;
+ int correct_4 = 2;
  cout<<"What is the capital of Japan?";
+ cout << " 1.Kyoto" << "\n 2. Tokyo " << "\n 3.Hiroshima" << endl;
  cin>>ans_4;
 
- string ans_5;
+ int ans_5;
+ int correct_5= 1;
  cout<<"what is the highest moutain in the world?";
+ cout << " 1.Everest" << "\n 2. Mt fuji " << "\n 3.K2" << endl;
  cin>>ans_5;
 }
-void Quiz::history()
-{
-     string ans_1;
-     cout<<"During the first times of exploration, the Western Hemisphere, in particular the Americas, were known by what term?";
-     cin>>ans_1;
 
-     string ans_2;
-     cout<<"During which war did Florence Nightingale attend to wounded soldiers?";
-     cin>>ans_2;
+void Quiz::science(){
+ int ans_1;
+ int correct_1 = 1;
+ cout<<"Who discovered the law of gravity?";
+ cout << " 1.Newton" << "\n 2. Einstein " << "\n 3.Galileo" << endl;
+ cin>>ans_1;
 
-     string ans_3;
-     cout<<"What name was given to the South African Wars of 1880-1881 and 1899-1902?";
-     cin>>ans_3;
+ int ans_2;
+ int correct_2 = 1;
+ cout<<"Which chemical element has the symbol He?";
+ cout << " 1.Hellium" << "\n 2. Hydrogen " << "\n 3.Carbon" << endl;
+ cin>>ans_2;
 
-     string ans_4;
-     cout<<"What British tribe did the Iceni belong to?";
-     cin>>ans_4;
+ int ans_3;
+ int correct_3 = 2;
+ cout<<"What is the name of main artery that carries blood from the heart?";
+ cout << " 1.coronary" << "\n 2. The Aorta " << "\n 3.Pulmonary" << endl;
+ cin>>ans_3;
 
-     string ans_5;
-     cout<<"During which World War did the Battle of the Somme occur?";
-     cin>>ans_5;
-}
-void Quiz::mixed()
-{
-     string ans_1;
-     cout<<"In computer science, what does ""GUI"" stand for?";
-     cin>>ans_1;
+ int ans_4;
+ int correct_4 = 1;
+ cout<<"Which two elements make up water";
+ cout << " 1.Hydrogen and oxygen " << "\n 2.Hydrogen and carbon" << "\n 3.Hydrogen and Hellium" << endl;
+ cin>>ans_4;
 
-     string ans_2;
-     cout<<"Who was the female lead in the movie ""Titanic""?";
-     cin>>ans_2;
-
-     string ans_3;
-     cout<<"What is the largest lake in Africa?";
-     cin>>ans_3;
-
-     string ans_4;
-     cout<<"Our solar system is located in what galaxy?";
-     cin>>ans_4;
-
-     string ans_5;
-     cout<<"What fast food franchise has the most worldwide locations?";
-     cin>>ans_5;
+ int ans_5;
+ int correct_5= 2
+ cout<<"Which vitamin does sunlight provide to humans?";
+ cout << " 1.Vitamin A" << "\n 2.Vitamin D" << "\n 3.Vitamin C" << endl;
+ cin>>ans_5;
 }
